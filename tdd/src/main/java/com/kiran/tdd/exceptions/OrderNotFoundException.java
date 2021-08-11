219b2d9bf9c63d5d5f99b67118c2eda55a9e69e6
kiran pawar <pawar_kiran@hotmail.com>
2021-08-11 20:19:06 +0530

    Controller Test added

--- tdd/src/main/java/com/kiran/tdd/exceptions/OrderNotFoundException.java
+++ tdd/src/main/java/com/kiran/tdd/exceptions/OrderNotFoundException.java
@@ -1,4 +1,8 @@
 package com.kiran.tdd.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class OrderNotFoundException extends RuntimeException {
 }
