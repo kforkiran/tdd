f6e17315d85b836763791dbe8a7cf8985d2484a3
kiran pawar <pawar_kiran@hotmail.com>
2021-08-14 22:15:37 +0530

    Removed unnecessary annotation

--- tdd/src/test/java/com/kiran/tdd/controller/OrderControllerTest.java
+++ tdd/src/test/java/com/kiran/tdd/controller/OrderControllerTest.java
@@ -4,20 +4,18 @@ import com.kiran.tdd.dto.Order;
 import com.kiran.tdd.exceptions.OrderNotFoundException;
 import com.kiran.tdd.service.OrderService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(OrderController.class)
-@ExtendWith(SpringExtension.class)
 class OrderControllerTest {
 
     @Autowired
