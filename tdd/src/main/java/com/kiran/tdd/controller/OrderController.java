7244aa37c95bc8164e367f2383ef5e7a939d2ac9
kiran pawar <pawar_kiran@hotmail.com>
2021-09-04 20:48:07 +0530

    refactored

--- tdd/src/main/java/com/kiran/tdd/controller/OrderController.java
+++ tdd/src/main/java/com/kiran/tdd/controller/OrderController.java
@@ -2,9 +2,7 @@ package com.kiran.tdd.controller;
 
 import com.kiran.tdd.dto.Order;
 import com.kiran.tdd.service.OrderService;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +24,6 @@ public class OrderController {
      @PostMapping("/orders")
      public ResponseEntity<Order> createOrder(@RequestBody Order order){
           final Order response = orderService.createOrder(order);
-          return ResponseEntity.status(CREATED).body(order);
+          return ResponseEntity.status(CREATED).body(response);
      }
 }
