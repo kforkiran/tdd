5fbb3650b4bf70bfddbe15f17b2143538561a0ce
kiran pawar <pawar_kiran@hotmail.com>
2021-09-04 20:50:15 +0530

    refactored

--- tdd/src/main/java/com/kiran/tdd/controller/OrderController.java
+++ tdd/src/main/java/com/kiran/tdd/controller/OrderController.java
@@ -6,24 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class OrderController {
 
-     @Autowired
-     private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-     @GetMapping("/orders/{id}")
-     public ResponseEntity<Order> getOrderDetails(@PathVariable("id") Long id){
-          Order order = orderService.getOrder(id);
-          return ResponseEntity.status(OK).body(order);
-     }
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<Order> getOrderDetails(@PathVariable("id") Long id) {
+        Order order = orderService.getOrder(id);
+        return ResponseEntity.status(OK).body(order);
+    }
 
-     @PostMapping("/orders")
-     public ResponseEntity<Order> createOrder(@RequestBody Order order){
-          final Order response = orderService.createOrder(order);
-          return ResponseEntity.status(CREATED).body(response);
-     }
+    @PostMapping("/orders")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        final Order response = orderService.createOrder(order);
+        return ResponseEntity.status(CREATED).body(response);
+    }
 }
