695ab2e31b176c6d65827fd96cf7e328d6d41f88
kforkiran <pawar_kiran@hotmail.com>
2021-09-06 21:01:33 +0530

    refactored

--- tdd/src/main/java/com/kiran/tdd/service/OrderService.java
+++ tdd/src/main/java/com/kiran/tdd/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.kiran.tdd.service;
 
 import com.kiran.tdd.dto.Order;
+import com.kiran.tdd.entities.OrderEntity;
 import com.kiran.tdd.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,21 @@ public class OrderService {
     }
 
     public Order createOrder(Order order) {
-        return null;
+        final OrderEntity newOrderEntity = orderRepository.save(toOrderEntity(order));
+        return toOrderDto(newOrderEntity);
+    }
+
+    private Order toOrderDto(OrderEntity newOrderEntity) {
+        return Order.builder()
+                .customerEmail(newOrderEntity.getCustomerEmail())
+                .customerAddress(newOrderEntity.getCustomerAddress())
+                .build();
+    }
+
+    private OrderEntity toOrderEntity(Order order) {
+        return OrderEntity.builder()
+                .customerEmail(order.getCustomerEmail())
+                .customerAddress(order.getCustomerAddress())
+                .build();
     }
 }
--- tdd/src/test/java/com/kiran/tdd/service/OrderServiceTest.java
+++ tdd/src/test/java/com/kiran/tdd/service/OrderServiceTest.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.any;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -34,10 +35,11 @@ class OrderServiceTest {
                 .quantity(9)
                 .build();
 
-        orderEntity.builder()
+        orderEntity = orderEntity.builder()
                 .customerEmail("pawar_kiran@live.in")
                 .customerAddress("Pune")
-                .lineItems(List.of(pens));
+                .lineItems(List.of(pens))
+                .build();
     }
 
     @Test
@@ -45,5 +47,16 @@ class OrderServiceTest {
         given(orderRepository.findById(1l)).willReturn(Optional.of(orderEntity));
         Order orderResponse = orderService.getOrder(1l);
         assertThat(orderResponse.getCustomerEmail()).isEqualTo("pawar_kiran@live.in");
+    }
+
+    @Test
+    void createOrder() {
+        given(orderRepository.save(any(OrderEntity.class))).willReturn(orderEntity);
+        final Order order = Order.builder()
+                .customerEmail("pawar_kiran@live.in")
+                .customerAddress("Pune")
+                .build();
+        final Order newOrder = orderService.createOrder(order);
+        assertThat(newOrder.getCustomerEmail()).isEqualTo("pawar_kiran@live.in");
     }
 }
